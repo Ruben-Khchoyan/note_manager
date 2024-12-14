4c2925c296895ea08647d78ffdd0d3fcb813af0e
Rubio_06 <101462136+Ruben-Khchoyan@users.noreply.github.com>
2024-12-14 17:24:51 +0300

    Add files via upload

--- data_changer.py
+++ data_changer.py
@@ -1,7 +1,8 @@
+
 from datetime import datetime as dt
 
 temp_issue_date = dt.strptime(input("Введите дату в формате ДД.ММ.ГГГГ: "), '%d.%m.%Y')
 temp_created_date = dt.strptime(input("Введите дату в формате ДД.ММ.ГГГГ: "), '%d.%m.%Y')
 
-print((str(temp_issue_date).split())[0])
-print((str(temp_created_date).split())[0])
+print((str(temp_issue_date).split())[0][5:])
+print((str(temp_created_date).split())[0][5:])
--- final.py
+++ final.py
@@ -6,14 +6,14 @@ infor = {'username':input("Введите Ваше имя: "),
          'status':input("Введите статус заметки: "),
          'created_date':dt.strptime(input("Введите дату создания в формате ДД.ММ.ГГГГ: "), '%d.%m.%Y'),
          'issue_date':dt.strptime(input("Введите дату в формате ДД.ММ.ГГГГ: "), '%d.%m.%Y'),
-         'title_1':[input("Введите заголовок 1: "), input("Введите заголовок 2: "), input("Введите заголовок 3: ")],
+         'title':[input("Введите заголовок 1: "), input("Введите заголовок 2: "), input("Введите заголовок 3: ")],
          }
 
 print("Имя пользователя:", infor['username'])
 print("Описание заметки:", infor['note'])
 print("Статус заметки:", infor['status'])
-print("Дата создания заметки:", (str(infor['created_date']).split())[0])
-print("Дата истечения заметки:", (str(infor['issue_date']).split())[0])
+print("Дата создания заметки:", (str(infor['created_date']).split())[0][5:])
+print("Дата истечения заметки:", (str(infor['issue_date']).split())[0][5:])
 
-for i in range(len(infor['title_1'])):
-    print(f'Заголовок {i+1} - {infor['title_1'][i]}' )
+for i in range(len(infor['title'])):
+    print(f'Заголовок {i+1} - {infor['title'][i]}' )
