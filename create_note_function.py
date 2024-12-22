c492f7ce8a887876ec5240e4b1ce540aa4226b96
Rubio_06 <101462136+Ruben-Khchoyan@users.noreply.github.com>
2024-12-22 17:52:06 +0300

    Add files via upload

--- create_note_function.py
+++ create_note_function.py
@@ -2,15 +2,12 @@
 from datetime import datetime as dt
 
 
-# Список notes для хранения всех заметок
-notes = []
-
-
-print('Добро пожаловать в "Менеджер заметок"! Вы можете добавить новую заметку.')
+# Список со всеми заметками
+lst_notes = []
 
 
 # Функция для проверки соответствия введенный даты требуемым форматам
-def check(per):
+def data_check(per):
     for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y', '%d-%m-%Y'):
         try:
             return dt.strptime(per, fmt)
@@ -19,10 +16,11 @@ def check(per):
     else:
         print('Введен неверный формат данных!')
         return 0
+# Конец функции проверки введенной даты
 
 
 # Функция для создания заметок
-def note():
+def create_note():
     # Цикл для введения имени пользователя и проверки чтобы не была пустая строка
     while True:
         user_name = input("Введите имя пользователя: ")
@@ -67,17 +65,13 @@ def note():
 
 
         # Цикл проверки формата ввденной даты создания
-        created_date = input("Введите дату создания заметки: ")
-        if check(created_date) != 0:
-            created_date = check(created_date)
-        else:
-            continue
+        created_date = dt.today()
 
 
         # Цикл проверки формата ввденной даты истечения
-        issue_date = input("Введите дату истечения заметки: ")
-        if check(issue_date) != 0:
-            issue_date = check(issue_date)
+        issue_date = input("Введите дату дедлайна: ")
+        if data_check(issue_date) != 0:
+            issue_date = data_check(issue_date)
         else:
             continue
 
@@ -91,16 +85,17 @@ def note():
 
 
     # Результат вывода фцнкции словарь со всеми данными заметок
-    return {'Имя пользователя':user_name,
-            'Заголовок':title,
-            'Описание':content,
-            'Статус':status,
-            'Дата создания':created_date.strftime('%d-%m-%Y'),
-            'Дедлайн':issue_date.strftime('%d-%m-%Y')}
+    return {'username':user_name,
+            'title':title,
+            'content':content,
+            'status':status,
+            'created_date':created_date.strftime('%d-%m-%Y'),
+            'issue_date':issue_date.strftime('%d-%m-%Y')}
+# Конец функции создания заметок
 
 
 # Добавление первой заметки
-notes.append(note())
+lst_notes.append(create_note())
 
 
 # Цикл для создания дополнительных заметок. В случае отказа цикл завершается.
@@ -109,16 +104,18 @@ while True:
     if create_new_note.strip().lower() not in ['да', 'нет']:
         print('\nНе корректный ответ!\n')
     elif create_new_note.strip().lower() == 'да':
-        notes.append(note())
+        lst_notes.append(create_note())
         continue
     elif create_new_note.strip().lower() == 'нет':
         break
 
 
-# Вывод всех заметок из списка notes в удобном формате.
-print("\nСписок заметок:")
-for i in range(len(notes)):
-    print(f'{i + 1}.', end='')
-    for k, v in notes[i].items():
-        print(f' {k}: {v}')
-    print()
+# Вывод всех заметок из списка lst_notes.
+if len(lst_notes) == 0:
+    print('Список заметок пуст!')
+elif len(lst_notes) == 1:
+    print('Заметка создана:\n', lst_notes[0])
+elif len(lst_notes) > 1:
+    print('Заметки созданы:')
+    for i in range(len(lst_notes)):
+        print(f'{i + 1}. {lst_notes[i]}')
